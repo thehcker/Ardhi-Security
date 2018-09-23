c07def5268f37d7fd4785c164a63baad303037bd
thehcker <40096385+thehcker@users.noreply.github.com>
2018-09-23 23:57:33 +0300

    Update settings.py

--- Box/settings.py
+++ Box/settings.py
@@ -139,15 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if DEBUG:
-    MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
-    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","media")
     STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(BASE_DIR),"static","static"),
+    os.path.join(os.path.dirname(BASE_DIR),"static"),
     )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
