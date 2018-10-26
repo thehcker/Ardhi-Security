310a306e9523816d22c29d7bb2cad276498915ce
thehcker <40096385+thehcker@users.noreply.github.com>
2018-10-27 02:50:07 +0300

    Update settings.py

--- Box/settings.py
+++ Box/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '*da#+)uenlo9eg@ln*=#@ob2#fye^qudjci70uuvhc6zylil@-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
